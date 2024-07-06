928be8386981aa5c77830f9c5b5c62f23545aa8f
Alex Chukrii <chukrii.alex@gmail.com>
2024-07-07 00:04:59 +0200

    Added html plugin of the webpack and propgress to see how quick the build will be made

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,19 @@
-const { cleanup } = require('@testing-library/react');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
+const webpack = require('webpack')
 
 module.exports = {
+  mode: 'development',
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'build'),
     filename: '[name].[contenthash].bundle.js',
+    clean: true
   },
-  clean: true
+  plugins: [
+    new webpack.ProgressPlugin(),
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, 'public', 'index.html')
+    })
+  ],
 };
